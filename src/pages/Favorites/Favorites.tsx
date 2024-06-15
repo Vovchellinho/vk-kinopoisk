c32f3b6f6fd876ba7ee8cd005424ffc7890ee1c4
vovhcellinho <ot4ertsov@gmail.com>
2024-06-15 17:45:06 +0300

    вывод списка избранного

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,9 +1,16 @@
-const Favorites = () => {
+import { observer } from "mobx-react-lite";
+import favoritesStore from "@store/favorites-store";
+import { useEffect } from "react";
+import FilmCard from "@components/FilmCard";
+
+const Favorites = observer(() => {
 	return (
-		<div>
-			Избранное
-		</div>
+		<ul>
+			{favoritesStore.favorites.map((movie) => <li key={`favorite-film-${movie.id}`}>
+				<FilmCard {...movie} />
+			</li>)}
+		</ul>
 	);
-};
+});
 
 export default Favorites;
